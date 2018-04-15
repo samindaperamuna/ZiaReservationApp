2ce111bb258b850090e9c656da9fd496d186ac31
Saminda Peramuna <saminda_lk@msn.com>
2018-04-15 21:05:25 +0530

    Updated reserve table dialog.

--- app/src/main/java/ziasrestaurant/com/reservationapp/ReserveActivity.java
+++ app/src/main/java/ziasrestaurant/com/reservationapp/ReserveActivity.java
@@ -1,13 +1,92 @@
 package ziasrestaurant.com.reservationapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ReserveActivity extends AppCompatActivity {
+
+    private final Calendar myCalendar = Calendar.getInstance();
+    private EditText dateEditText;
+    private EditText timeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserve);
+
+        dateEditText = findViewById(R.id.dateEditText);
+        timeEditText = findViewById(R.id.timeEditText);
+
+        initDatePicker();
+        initTimePicker();
+    }
+
+    private void initDatePicker() {
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDateLabel();
+            }
+        };
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(ReserveActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void initTimePicker() {
+
+        final TimePickerDialog.OnTimeSetListener time = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                myCalendar.set(Calendar.MONTH, minute);
+                updateTimeLabel();
+            }
+        };
+
+        timeEditText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                new TimePickerDialog(ReserveActivity.this, time, myCalendar
+                        .get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE),
+                        DateFormat.is24HourFormat(ReserveActivity.this)).show();
+            }
+        });
+    }
+
+    private void updateDateLabel() {
+        String myFormat = "MM/dd/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dateEditText.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void updateTimeLabel() {
+        String myFormat = "HH:mm";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        timeEditText.setText(sdf.format(myCalendar.getTime()));
     }
 }
