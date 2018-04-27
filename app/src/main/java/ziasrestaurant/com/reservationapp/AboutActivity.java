9f9902d5fcbbf03d2954f011e038962ee03cc7ca
Saminda Peramuna <saminda_lk@msn.com>
2018-04-28 00:34:05 +0530

    About page changes.

--- app/src/main/java/ziasrestaurant/com/reservationapp/AboutActivity.java
+++ app/src/main/java/ziasrestaurant/com/reservationapp/AboutActivity.java
@@ -1,13 +1,31 @@
 package ziasrestaurant.com.reservationapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ImageButton facebookImageButton = findViewById(R.id.facebookImageButton);
+        facebookImageButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.facebookImageButton) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+            intent.setData(Uri.parse(getString(R.string.facebook_link)));
+            startActivity(intent);
+        }
     }
 }
